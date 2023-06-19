54539856f693a9a17dbb16017b298b0f1e9bf281
DevZenBook <julien.mangeat@mail-formateur.net>
2023-06-19 09:39:08 +0200

    init projet

---
+++ lib/models/expense.dart
@@ -0,0 +1,31 @@
+class Expense {
+  final int id;
+  final String description;
+  final double amount;
+  final DateTime date;
+
+  Expense({
+    required this.id,
+    required this.description,
+    required this.amount,
+    required this.date,
+  });
+
+  factory Expense.fromMap(Map<String, dynamic> map){
+    return Expense(
+      id: map['id'],
+      description: map['description'],
+      amount: map['amount'],
+      date: DateTime.parse(map['date']),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'description': description,
+      'amount': amount,
+      'date': date.toIso8601String(),
+    };
+  }
+}
